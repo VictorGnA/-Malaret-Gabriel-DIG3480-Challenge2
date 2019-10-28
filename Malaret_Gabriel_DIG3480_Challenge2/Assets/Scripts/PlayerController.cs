d7aa50defd8060e69056f06a9e88751b24474c47
Gabe Malaret <44530082+VictorGnA@users.noreply.github.com>
2019-10-27 23:56:22 -0400

    Challenge2

---
+++ Malaret_Gabriel_DIG3480_Challenge2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject player;
+    public Text countText;
+    public Text livesText;
+    public Text winText;
+    public Text loseText;
+    public Transform TP;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+
+        count = 0;
+
+        lives = 3;
+
+        winText.text = "";
+
+        loseText.text = "";
+
+        SetCountText();
+
+        SetLivesText();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // This should affect the players lives
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+
+            lives = lives - 1;
+
+            SetCountText();
+            SetLivesText();
+        }
+        // Intended to have player be turned off
+        if (lives <= 0)
+            player.gameObject.SetActive(false);
+
+        if (other.gameObject.tag == "Teleport")
+        {
+            transform.position = TP.position;
+        }
+
+        // This needs the tp to spawn at location
+        // if (count <= 11)
+        // {
+            //other.gameObject.CompareTag("Teleport");
+            //other.gameObject.SetActive(false);
+
+            //if (count >= 12)
+            //{
+
+              //  other.gameObject.CompareTag("Teleport");
+              //  other.gameObject.SetActive(true);
+            //}
+        //}
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Pickup")
+        {
+            count += 1;
+            countText.text = count.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 5)
+            winText.text = "You Win! Game Created" +
+                "By Gabriel Malaret";
+
+    }
+    // Text should be display after lives counter goes to 0
+    void SetLivesText() {
+        livesText.text = "Lives: " + lives.ToString();
+
+        if (lives <= 0)
+            loseText.text = "You Lose!";
+       
+
+    }
+    
+
+    
+
+
+
+
+
+
+
+}
+
---
+++ Malaret_Gabriel_DIG3480_Challenge2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public bool facingRight = false;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+       float hozMovement = Input.GetAxis("Horizontal");
+
+            if (hozMovement < 0.0f && facingRight == false)
+                {
+                    FlipPlayer();
+                }
+            else if (hozMovement > 0.0f && facingRight == true)
+                {
+                    FlipPlayer();
+                }
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+    }
+
+    void FlipPlayer ()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
